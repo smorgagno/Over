d419a2c30a656e93f0877b81be91c078d2b01b8e
smorgagno <48386487+smorgagno@users.noreply.github.com>
2024-04-07 14:13:15 +0200

    updates

--- Over/Over.py
+++ Over/Over.py
@@ -18,11 +18,11 @@ class Over(hass.Hass):
             stato = self.get_state(entity)
             message = "{} changed to {}".format(self.friendly_name(entity), new)
             self.log(message)
-            self.invia_notifiche(message)
+            self.send_notifications(message)
     
     ## invio notifiche a tutti i dispositivi previsti nel file di configurazione
-    def invia_notifiche(self, messaggio):
-        for servizio_notifica in self.args["notification_services"]:
-            service = "notify/" + servizio_notifica
-            self.call_service(service, title = "Konnected:", message = messaggio)
+    def send_notifications(self, messaggio):
+        for service_name in self.args["notification_services"]:
+            service = "notify/" + service_name
+            self.call_service(service, title = "Over:", message = messaggio)
         return
